ae09fb0432d6007bed0c08aa5daea1afc389d2c8
HOUAYS Yassine <Yass76300@gmail.com>
2020-02-20 19:30:45 +0100

    add : Auth authentification

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
 const server = require('http').createServer(app);
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');  
+const passport = require('passport');
 
 
 app.set('views', __dirname + '/views');
@@ -14,8 +15,45 @@ app.use(bodyParser.json());
 // Router
 const routes = require('./routes/routes');
 app.use('/', routes);
+var authentification = require('./routes/authentification');
+app.use('/authentification/', authentification);
 
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+				var config = require('./oauth.js');
 
+				passport.use('google', new GoogleStrategy({
+				      clientID        : config.google.clientID,
+				      clientSecret    : config.google.clientSecret,
+				      callbackURL     : config.google.callbackURL,
+				      profileFields: ['id', 'emails', 'name']
+				    },
+
+				    function(access_token, refresh_token, profile, done) {
+				      process.nextTick(function() {
+				        Account.findOne({ 'username' : profile.emails[0].value }, function(err, user) {
+				          if (err)
+				            return done(err);
+				          if (user) {
+				            if (err)
+				              return done(err);
+				            else
+				              return done(null, user);
+				          } else {
+				            if (err)
+				              return done(err);
+
+				            Account.register(new Account({ username : profile.emails[0].value }), profile.id, function(err, account) {
+				              if (err)
+				                return done(err);
+				              else
+				                return done(null, account);
+				            });
+				          }
+				        });
+				      });
+				    }
+				));
+			
 
 server.listen(3030, () => {
     console.log(`Listening to requests on http://localhost:3030`);
---
+++ oAuth.js
@@ -0,0 +1,9 @@
+var id = {
+    google: {
+        clientID: '321839277112-3djijtujbork4l4lid8eku1isna4ciep.apps.googleusercontent.com',
+        clientSecret: 'iYJb7ZvL4DsYn-6S_K4hi7ER',
+        callbackURL: 'http://localhost:3000/authentification/google/callback'
+    }
+};
+
+module.exports = id;
